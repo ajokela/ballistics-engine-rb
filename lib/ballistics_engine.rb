d9959a626f3bec1b50aa6b445a1e7391e0ac777b
Alex Jokela <alex@camulus.com>
2026-07-12 20:50:53 -0500

    Bump to 0.23.0 (engine 0.23.0: custom drag tables, canted rifle, moving-target lead)

--- lib/ballistics_engine.rb
+++ lib/ballistics_engine.rb
@@ -4,7 +4,7 @@ require "ballistics_engine_rb"
 
 # Top-level module for the BallisticsEngine gem
 module BallisticsEngine
-  VERSION = "0.22.11"
+  VERSION = "0.23.0"
 
   class Error < StandardError; end
 end
