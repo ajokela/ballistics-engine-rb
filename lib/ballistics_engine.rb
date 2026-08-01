598e33caae950b4e1a7b0b1849c50dbf11670985
Alex Jokela <alex@camulus.com>
2026-08-01 11:36:59 -0500

    Release 0.32.0 (track ballistics-engine 0.32.0)

--- lib/ballistics_engine.rb
+++ lib/ballistics_engine.rb
@@ -4,7 +4,7 @@ require "ballistics_engine_rb"
 
 # Top-level module for the BallisticsEngine gem
 module BallisticsEngine
-  VERSION = "0.31.0"
+  VERSION = "0.32.0"
 
   class Error < StandardError; end
 end
