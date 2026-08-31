fcee15990c7d10dff24bd086cbdd54463e07c3de
Alex Jokela <alex@camulus.com>
2026-08-31 10:46:23 -0500

    Bump to 0.36.0 (engine 0.36.0)

--- lib/ballistics_engine.rb
+++ lib/ballistics_engine.rb
@@ -4,7 +4,7 @@ require "ballistics_engine_rb"
 
 # Top-level module for the BallisticsEngine gem
 module BallisticsEngine
-  VERSION = "0.35.0"
+  VERSION = "0.36.0"
 
   class Error < StandardError; end
 end
